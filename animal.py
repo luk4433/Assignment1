a6c41291ee65832f91f9cf37d66a4df124948494
Luk4433 <lukegordon57@gmail.com>
2022-09-20 23:09:19 -0600

    Commit

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Luke Gordon
+# GitHub username: luk4433
+# Date: 09/20/2022
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
